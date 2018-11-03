f00e4999210846d0baaf4ecde37a9198a50575f9
Vina <vina_melody@yahoo.com>
2018-11-03 13:33:25 +0800

    Initial Commit

---
+++ iosconfsg2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosconfsg2019
+//
+//  Created by Vina Melody on 3/11/18.
+//  Copyright © 2018 Vina Melody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
