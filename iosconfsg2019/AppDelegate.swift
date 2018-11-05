1ea3ec59c396d48b731e43e420d51c6e8e305f04
Vina <vina_melody@yahoo.com>
2018-11-05 22:13:27 +0800

    Replace storyboard with HomeViewController

--- iosconfsg2019/AppDelegate.swift
+++ iosconfsg2019/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+
         return true
     }
 
--- iosconfsg2019/HomeViewController.swift
+++ iosconfsg2019/HomeViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.white
+        
     }
 
 
