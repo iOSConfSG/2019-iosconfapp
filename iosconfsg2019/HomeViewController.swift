e3431671aa165e2999d40d75ec562793f143f9e4
Vina <vina_melody@yahoo.com>
2018-11-10 19:19:17 +0800

    Add segmented control

--- iosconfsg2019/HomeViewController.swift
+++ iosconfsg2019/HomeViewController.swift
@@ -10,17 +10,45 @@ import UIKit
 
 class HomeViewController: UITableViewController {
     
+    let daySegmentControl = UISegmentedControl(items: ["Day 1", "Day 2"])
+    
     private let timelineCellId: String = "timelineCell"
     private let detailCellId: String = "detailCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.white
+        setupViews()
         
         tableView.register(TimelineCell.self, forCellReuseIdentifier: timelineCellId)
         tableView.register(DetailCell.self, forCellReuseIdentifier: detailCellId)
         
+    }
+    
+    // MARK: - Private method
+    private func setupViews() {
+        self.view.backgroundColor = UIColor.white
+        
+        self.daySegmentControl.selectedSegmentIndex = activeSegmentIndex()
+        self.daySegmentControl.addTarget(self, action: #selector(handleChangeDay), for: .valueChanged)
+        self.daySegmentControl.tintColor = UIColor.purple
+        self.navigationItem.titleView = self.daySegmentControl
+    }
+    private func activeSegmentIndex() -> Int {
+        let today = Date()
+        let calendar = Calendar.current
+        let component = calendar.dateComponents([.day], from: today)
+        
+        // Todo: get from config
+        if component.day == 17 {
+            return 0
+        } else {
+            return 1
+        }
+    }
+    
+    @objc private func handleChangeDay() {
+        let selectedIndex = self.daySegmentControl.selectedSegmentIndex
+        print("Switch to \(selectedIndex)")
     }
 
     
@@ -40,6 +68,7 @@ class HomeViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: timelineCellId) as! TimelineCell
+        cell.selectionStyle = .none
         return cell
     }
 
