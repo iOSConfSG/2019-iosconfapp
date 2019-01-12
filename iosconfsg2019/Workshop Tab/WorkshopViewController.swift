51f3dfc7dfd4904df8a2cba1abbe9b09b4c55626
Kale Toh <kale@kalestarler.com>
2019-01-13 01:20:00 +0700

    Changed the attributed text view as subview to tableheaderview, reduced hardcoded height

--- iosconfsg2019/Workshop Tab/WorkshopViewController.swift
+++ iosconfsg2019/Workshop Tab/WorkshopViewController.swift
@@ -49,18 +49,39 @@ class WorkshopViewController: UIViewController {
         self.view.backgroundColor = UIColor.white
         self.navigationItem.title = "Workshop"
         
-        self.view.addSubview(locationView)
+//        self.view.addSubview(locationView)
         self.view.addSubview(tableView)
         
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.tableHeaderView = containerView
+        
+        locationView.attributer =
+            "Workshop Location".purple.font(UIFont.boldSystemFont(ofSize: UIFont.largeSize))
+                .append("\n\n")
+                .append("PlugIn@BLK71\n")
+                .append("71 Ayer Rajah Crescent, 02-18, Singapore 139951\n")
+                .append("https://goo.gl/maps/vZB8otAC5yL2").matchLinks.makeInteract({ (link) in
+                    guard let url = URL(string: link) else {
+                        return
+                    }
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                })
+                .append("\n\nWorkshop Schedule").purple.font(UIFont.boldSystemFont(ofSize: UIFont.largeSize))
+        
+        containerView.addSubview(locationView)
+        
         NSLayoutConstraint.activate([
-            locationView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0),
-            locationView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0),
-            locationView.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor, constant: 0),
-            locationView.heightAnchor.constraint(equalToConstant: 140),
-            tableView.topAnchor.constraint(equalTo: locationView.bottomAnchor, constant: 10),
+            containerView.widthAnchor.constraint(equalTo: self.tableView.widthAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: 125),
+            containerView.topAnchor.constraint(equalTo: self.tableView.topAnchor, constant: 0),
+            tableView.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor, constant: 0),
             tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0),
             tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0),
-            tableView.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.topAnchor, constant: 0)
+            tableView.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.topAnchor, constant: 0),
+            locationView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
+            locationView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0),
+            locationView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0),
             ])
         
         tableView.delegate = self
@@ -68,18 +89,6 @@ class WorkshopViewController: UIViewController {
         self.tableView.rowHeight = UITableView.automaticDimension
         self.tableView.estimatedRowHeight = 80
         
-        locationView.attributer =
-            "Workshop Location".purple.font(UIFont.boldSystemFont(ofSize: UIFont.largeSize))
-            .append("\n\n")
-            .append("PlugIn@BLK71\n")
-            .append("71 Ayer Rajah Crescent, 02-18, Singapore 139951\n")
-                .append("https://goo.gl/maps/vZB8otAC5yL2").matchLinks.makeInteract({ (link) in
-                    guard let url = URL(string: link) else {
-                        return
-                    }
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                })
-            .append("\n\nWorkshop Schedule").purple.font(UIFont.boldSystemFont(ofSize: UIFont.largeSize))
     }
     
     private func getSchedule() {
