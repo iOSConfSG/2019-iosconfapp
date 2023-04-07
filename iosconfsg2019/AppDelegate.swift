f97dc69579677be84219d73279ec8dbf1a49b222
Vina Melody <vina_melody@yahoo.com>
2023-04-07 18:10:02 +0800

    Add Flagsmith

--- iosconfsg2019/AppDelegate.swift
+++ iosconfsg2019/AppDelegate.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import FlagsmithClient
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     private var modules: [UIApplicationDelegate] = [UIApplicationDelegate]()
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         registerModules()
-
+        
         _ = modules.compactMap {
             _ = $0.application?(application, didFinishLaunchingWithOptions: launchOptions)
         }
@@ -33,12 +34,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = CustomTabBarController()
         return true
     }
-
+    
 }
 
 extension AppDelegate {
     private func registerModules() {
-        // Nothing to append
+        
+        if let flagsmithKey = ProcessInfo.processInfo.environment["FLAGSMITH_KEY"] {
+            Flagsmith.shared.apiKey = flagsmithKey
+            
+            Flagsmith.shared.getFeatureFlags { result in
+                switch result {
+                case .success(let flags):
+                    for flag in flags {
+                        let name = flag.feature.name
+                        let value = flag.value.stringValue
+                        let enabled = flag.enabled
+                        print(name, "= enabled:", enabled, "value:", value ?? "nil")
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
     }
 }
 
